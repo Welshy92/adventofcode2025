87597eb795993c3c68e67b5057399e656d84dc21
Jacob Welsh <welshy92@outlook.com>
2026-04-16 17:27:12 +0100

    CLOSE to correct

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,49 +1,25 @@
-zero_counter = 0
-new_num = 0
+with open("input.txt", 'r') as f:
+	# dial = f.readlines()
+	dial = list(map(int, f.read().replace('R', '').replace('L', '-').split()))
 
+### PART 1 ###
+zero_count = 0
+x = 50
+for turn in dial:
+	x += turn
+	if x % 100 == 0:
+		zero_count += 1
+print(f'part 1:\t{zero_count}') #This reads 1036 which is correct.
 
-def grabEntry():
-    lines_list = []
-
-    with open("input.txt", "r") as file:
-        lines_with_newlines = file.readlines()
-
-    for line in lines_with_newlines:
-        lines_list.append(line.strip())
-    
-    return lines_list
-
-
-def makeSum(startnum, direction, endnum):
-    global zero_counter
-    global new_num
-
-    if direction == "L":
-        new_num = startnum - endnum
-    else:
-        new_num = startnum + endnum
-    
-    if new_num >= 100:
-        new_num = new_num - 100
-        zero_counter = zero_counter + 1
-    elif new_num <= 0:
-        new_num = new_num + 100
-        zero_counter = zero_counter + 1
-
-
-def main():
-
-    data_list = grabEntry()
-
-    makeSum(50, "R", 55)
-    print("Current Number = " + str(new_num))
-    print("zero_counter = " + str(zero_counter))
-
-    makeSum(new_num, "L", 25)
-    print("Current Number = " + str(new_num))
-    print("zero_counter = " + str(zero_counter))
-
-    print(data_list)
-
-
-main()
+### PART 2 ###
+zero_count = 0
+x = 50
+for turn in dial:
+	zero_count += abs(turn) // 100
+	a = 1 if turn > 0  else -1
+	rem = a * abs(turn % 100)
+	x += rem
+	if x >= 100 or (x <= 0 and x != rem):
+		zero_count += 1
+	x %= 100
+print(f'part 2:\t{zero_count}') # This reads 6193 which is incorrect. It is too low.
