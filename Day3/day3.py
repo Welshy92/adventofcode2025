84d895ab2942166d7d7cd7c59b2b06f1b3367a01
Welshy92 <welshy92@outlook.com>
2026-04-18 05:38:28 +0100

    ADD initial thought on how to solve day 3.

---
+++ Day3/day3.py
@@ -0,0 +1,7 @@
+# NOTES TO TRY. Read line by line in the data.
+# Can either try finiding the max number if that's possbile. Do that twice.
+# Or can just split all numbers into seperate array entries then do an iterative comparison to see which 1 if bigger.
+# Keep the index of the larger number noted down then do it again but either starting from the index of the larger
+# number or remove all the numbers that don't need to be checked for the 2nd time.
+
+# Once the 2 numbers are found, join them together to make 1 and then add it to the overall total.
