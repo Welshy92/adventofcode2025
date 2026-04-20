178eb1583eac58860100508f439945bbc7447b0f
Welshy92 <welshy92@outlook.com>
2026-04-20 15:08:04 +0100

    ADD day 4 first thoughts notes

---
+++ Day4/day4.py
@@ -0,0 +1,6 @@
+# Split each line into it's own array (they all all equal length)
+# Iterate through each line and then do a for loop on each '@', check the index before and after and then
+# check the before, after AND equal index on the previous and next array index entry.
+
+# For each additional '@' found, add 1 to the count. If the total is greater or equal to 5 (including the original '@' then do not add to the total count.)
+# Otherwise, add 1 to the total count.
